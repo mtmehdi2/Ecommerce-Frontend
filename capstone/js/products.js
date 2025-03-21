c5b5ce941e91b979d533f611f4e25722fc5333b9
Mehdi-Win-Loudoun <makbari@mwcog.org>
2025-03-21 12:30:46 -0400

    Updated products.html page

---
+++ capstone/js/products.js
@@ -0,0 +1,24 @@
+Promise.all([
+  fetch("http://3.136.18.203:8000/products/").then((res) => res.json()),
+  fetch("http://3.136.18.203:8000/categories/").then((res) => res.json()),
+])
+  .then(([products, categories]) => {
+    products.map((item) => {
+      const category = categories.find(
+        (cat) => cat.category_id === item.category
+      );
+      price_str = item.starting_at_price.toString().split(".");
+      _("#products-container").innerHTML += `
+        <a class='prod-lnk' href='product.html?product_id=${item.product_id}'>
+        <div class='prod-container'>
+            <div class='prod-img-ctnr'><img class='prod-img' src='${item.picture_url}' alt='' width='500' height='500'></div>
+            <h2 class='prod-titlle'>${item.name}</h2>
+            <div class='prod-category'>${category.name}</div>
+            <div class='prod-quantity'>Quantity: ${item.stock_quantity}</div>
+            <div class='prod-price'>$${item.starting_at_price}</div>
+        </div></a>`;
+    });
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });

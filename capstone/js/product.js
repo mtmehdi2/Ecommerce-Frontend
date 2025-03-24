5d4500f616c9d866a925be77929db6c879a052cd
Mehdi-Win-Loudoun <makbari@mwcog.org>
2025-03-24 08:29:32 -0400

    Implemented shopping cart.

--- capstone/js/product.js
+++ capstone/js/product.js
@@ -5,6 +5,7 @@ const apiUrl = "http://3.136.18.203:8000/products/" + prodId + "/";
 const catUrl = "http://3.136.18.203:8000/categories/";
 const template = _("#prod-template");
 const ctnr = _("#product-container");
+const d = document;
 
 Promise.all([
   fetch(apiUrl).then(handleErrors),
@@ -37,13 +38,66 @@ Promise.all([
     myT.querySelector(".p-price").innerHTML += price_html;
     myT.querySelector("select").innerHTML = variety_options;
     _("#product-container").appendChild(myT);
+    _("#product_id").value=prodId;
+    updatePrice();
   })
   .catch((err) => {
     showError(err.message);
   });
 
-const updatePrice = (cost) => {
-  cost_str = cost.toString().split(".");
+const updatePrice = () => {
+  const varieties = d.querySelector('#varieties');
+  const cost = varieties.options[varieties.selectedIndex].value;
+  const qty=d.querySelector('#quantity').value;
+  const total = Number(qty)*Number(cost);
+  d.querySelector('#variety').value = varieties.options[varieties.selectedIndex].innerText.split("|")[0].trim();
+  d.querySelector('#price').value=cost;
+  d.querySelector('#total_price').value=total;
+  cost_str = total.toString().split(".");
   cost_html = "$" + cost_str[0] + "<sup>" + cost_str[1] + "</sup>";
   _(".p-price")[0].innerHTML = cost_html;
 };
+
+const addToCart = ()=>{
+  const prod_id = d.querySelector('#product_id').value;
+  const qty = d.querySelector('#quantity').value;
+  const variety = d.querySelector('#variety').value;
+  const unit_price = d.querySelector('#price').value;
+  const total_price= d.querySelector('#total_price').value;
+  const cartItem = {
+    'product_id': prod_id,
+    'variety': variety,
+    'quantity': qty,
+    'price':unit_price,
+    'total':total_price
+  };
+  
+  const currCartItems = localStorage.getItem("capstoneShoppingCart");
+  if (currCartItems===null || currCartItems===undefined){
+    const nCart = [];
+    nCart.push(cartItem);
+    localStorage.setItem("capstoneShoppingCart",JSON.stringify(nCart));
+    alert(_("#prod-title").innerText+' has been added to the cart successfully');
+  }else{
+    const cart=JSON.parse(currCartItems);
+    let currQty=0;
+    for(x of cart){
+      if(x.product_id == prod_id && x.variety == variety){
+        currQty = x.quantity;
+      }
+    }
+    let newCart = [];
+    for (i = 0; i< cart.length; i++){
+      if(cart[i].product_id != prod_id || cart[i].variety!=variety){
+        newCart.push(cart[i]);
+      }
+    }
+    const newQty = Number(qty)+Number(currQty);
+    cartItem.quantity=newQty;
+    cartItem.total = Number(cartItem.quantity)*Number(cartItem.price);
+    newCart.push(cartItem);
+    localStorage.setItem('capstoneShoppingCart',JSON.stringify(newCart));
+    alert(_("#prod-title").innerText+' has been added to the cart successfully');
+  }
+
+}
